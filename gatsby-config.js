f521163c62e460c3e7a3b0886d886ea9c57fb859
Jon <jon@rivalsouth.com>
2021-06-12 12:54:34 +0100

    Added Gatsby Cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@shirlierandall`,
   },
   plugins: [
+    `gatsby-plugin-gatsby-cloud`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
